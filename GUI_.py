12879ebfdc83632346b6b0bfb0888c733ffc8963
Shruthi <shrutzdabest@gmail.com>
2013-12-05 18:07:50 +0800

    fixed GUI
    Need to test if functioning with headband

--- GUI_.py
+++ GUI_.py
@@ -1,4 +1,4 @@
-from pyemotiv import emotiv
+from pyemotiv import Epoc
 import numpy as np
 from PyQt4 import QtCore, QtGui, Qt
 from PyQt4.QtGui import QApplication, QMainWindow
@@ -8,37 +8,53 @@ import sys
 from GUImain import Ui_MainWindow
 import time
 
-class QtEEG(QtCore.QObject):
 
-	#need to add the acquisition module in here 
+
+class QtEEG(QtCore.QObject):
 	finished_adq = QtCore.pyqtSignal()
+	#need to add the acquisition module in here 
+	
 	def __init__(self,mainwindow):
 		super(QtEEG, self).__init__()
-				
-		# Emotiv Initialization
-		self.clab = ['F3', 'F4', 'P7', 'FC6', 'F7', 'F8', 'T7', 'P8', 
-							'FC5', 'AF4','T8', 'O2', 'O1', 'AF3']
-		self.timek = 512
+        
+
+	def initUI(self): 
+		
+		# Emotiv Initialization and data acquisition
+		
+		epoc= Epoc()
+
+		# collecting data from the headset and placing into two different arrays
+
+		data= epoc.aquire([9]) 
+		#data2= epoc.aquire([16]) 
+		datarray = np.concatenate((datarray, data), axis = 1)
+		#datarray2= np.concatenate((datarray2, data2), axis=1)
+
+
+
+		#self.clab = ['F3', 'F4', 'P7', 'FC6', 'F7', 'F8', 'T7', 'P8', 'FC5', 'AF4','T8', 'O2', 'O1', 'AF3']
+		self.timek = 512 
 		self.x = np.arange(0,self.timek,1)
-		self.y = [[0] for i in xrange(len(self.clab))]
-		self.offset = [2500.0*i for i in xrange(len(self.clab))]
+		self.y = [[0] for i in xrange(len(self.datarray[0]))]
+		self.offset = [2500.0*i for i in xrange(len(self.datarray[0]))]
 		self.mutex_stop = Qt.QMutex()
 		self.cancel = False		
 	
-	def addTimeSeries(self,clab):
+	def addTimeSeries(self,datarray):
 		self.plt = []
-		for i in xrange(len(clab)):
-			self.plt.append(Qwt.QwtPlotCurve(clab[i]))
+		for i in xrange(len(datarray[0])):
+			self.plt.append(Qwt.QwtPlotCurve(datarray[0][i]))
 			self.plt[i].attach(self.mainwindow.eegPlot)
 			self.plt[i].setPen(Qt.QPen(Qt.Qt.red))	
 	
 	def plotEEG(self):	
-		import gevent
-		from gevent import monkey		
-		self.headset = emotiv.Emotiv()
-		monkey.patch_all(True)
+		#import gevent
+		#from gevent import monkey		
+		#self.headset = emotiv.Emotiv()
+		#monkey.patch_all(True)
 		print('Plotting!!!!')		
-		gevent.spawn(self.headset.setup)
+		#gevent.spawn(self.headset.setup)
 		while True:
 			self.mutex_stop.lock()
 			if self.cancel:
@@ -46,8 +62,8 @@ class QtEEG(QtCore.QObject):
 			self.mutex_stop.unlock()			
 			self.packet = self.headset.dequeue()
 			if self.packet is not None:
-				for i in xrange(len(self.clab)):
-					p = self.packet.sensors[self.clab[i]]
+				for i in xrange(len(self.datarray[0])):
+					p = self.packet.sensors[self.datarray[0][i]]
 						
 					self.y[i].append(p['value'])
 					if len(self.y[i]) > self.timek:
@@ -71,18 +87,12 @@ class QtEEG(QtCore.QObject):
 			
 
 def main():
-
-	#shows the GUI
-	app = QtGui.QApplication([])
+	app = QtGui.QApplication(sys.argv)
+	
 	window = QtGui.QMainWindow()
 	ui = Ui_MainWindow()
 	ui.setupUi(window)
 	window.show()
-
-
-
-if __name__ == '__main__':
-
 	emotivObj = QtEEG(ui)
 	adqThread = QtCore.QThread()
 	emotivObj.moveToThread(adqThread)
@@ -93,5 +103,12 @@ if __name__ == '__main__':
 	ui.stopEEG.clicked.connect(emotivObj.stop)
 	
 	sys.exit(app.exec_())
+
+
+
+if __name__ == '__main__':
+	main()
+
+	
 	
 	
