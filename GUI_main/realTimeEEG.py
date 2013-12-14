19a4a602b12b26010aa6a0d5be8fa495783eaed4
Shruthi <shrutzdabest@gmail.com>
2013-12-14 17:24:55 +0800

    GUI_completely_fixed
    Need to add O2 channel

--- GUI_main/realTimeEEG.py
+++ GUI_main/realTimeEEG.py
@@ -1,108 +1,98 @@
-import ui_plot_form 
+import ui_plot_form
 from recorder import *
 import sys
 import numpy
 from PyQt4 import QtCore, QtGui
 import PyQt4.Qwt5 as Qwt
-from pyemotiv import Epoc 
+from pyemotiv import Epoc
 
 
-#epoc=Epoc()
+# epoc=Epoc()
 
 def plotSomething():
-    if epoc.newRecord==False: 
+    if epoc.newRecord == False:
         return
-    #how do I get the code in here? Need to print the raw EEG data 
-    #click a button and it stops updating it
-    #click another button. it generates FFT signal
-    #print "\n Plotting"
-    #ys_arr= []
-    xs,ys=epoc.rawdata()
+    # how do I get the code in here? Need to print the raw EEG data
+    # click a button and it stops updating it
+    # click another button. it generates FFT signal
+    # print "\n Plotting"
 
-    #print ys
-     
-    #print (ys_arr)
+    xs = numpy.arange(0, 511)
 
-    c.setData(xs, ys)
-    
+    # print ys
+
+    c.setData(xs, epoc.ys)
     uiplot.qwtPlot.replot()
-    epoc.newRecord=False
-    
-        
-def contplot(): 
-    win_plot.connect(uiplot.timer, QtCore.SIGNAL('timeout()'), plotSomething)   
+
+    epoc.newRecord = False
 
 
+def contplot():
+    win_plot.connect(uiplot.timer, QtCore.SIGNAL('timeout()'), plotSomething)
 
-def fftplot(): 
-    if epoc.newRecord==False: 
-        return 
 
-    freq,pwr= epoc.fft()
-    d.setData(freq,pwr)
-    uiplot.qwtPlot.replot()
-    epoc.newRecord=False
+def fftplot():
+    if epoc.newRecord == False:
+        return
 
-def contfftplot(): 
+    freq, pwr = epoc.fft()
+    d.setData(freq, pwr)
+    uiplot.qwtPlot_2.replot()
+    epoc.newRecord = False
+
+
+def contfftplot():
     win_plot.connect(uiplot.timer, QtCore.SIGNAL('timeout()'), fftplot)
 
 
-
-
-def main(): 
+def main():
     app = QtGui.QApplication(sys.argv)
     global epoc
-    global c,d
+    global c, d
     global uiplot, win_plot
-    
 
-    epoc=EEGRecorder()
-    
-    #epoc.setup
-    #print"boom"
-    epoc.continuousStart() #should start the record function
-    #print "Hi"
+    epoc = EEGRecorder(9)
+    epoc2 = EEGRecorder(10)
+
+    # epoc.setup
+    # print"boom"
+    epoc.continuousStart()  # should start the record function
+    # print "Hi"
 
     win_plot = ui_plot_form.QtGui.QMainWindow()
     uiplot = ui_plot_form.Ui_Form()
     uiplot.setupUi(win_plot)
-   
-    uiplot.btn2.clicked.connect(epoc.continuousEnd) #when buttonB is clicked, want it to stop
+
+    #when buttonB is clicked, want it to stop
+    uiplot.btn2.clicked.connect(epoc.continuousEnd)
     uiplot.btn1.clicked.connect(contplot)
     uiplot.btn3.clicked.connect(contfftplot)
-    uiplot.pushButton_2.clicked.connect(epoc.batt)
-  
-    c=Qwt.QwtPlotCurve()  
-    c.attach(uiplot.qwtPlot) #attaching the qwtplot to the object 
+    # uiplot.pushButton_2.clicked.connect(epoc.batt)
 
-    d=Qwt.QwtPlotCurve()
+    c = Qwt.QwtPlotCurve()
+    c.attach(uiplot.qwtPlot)  # attaching the qwtplot to the object
+
+    d = Qwt.QwtPlotCurve()
     d.attach(uiplot.qwtPlot_2)
 
-    #fixing the axes for the 2 plots
+    # fixing the axes for the 2 plots
 
-    uiplot.qwtPlot.setAxisScale(uiplot.qwtPlot.xBottom,0,500) 
-    uiplot.qwtPlot.setAxisScale(uiplot.qwtPlot.yLeft, 3000,6000) 
-    uiplot.qwtPlot_2.setAxisScale(uiplot.qwtPlot.xBottom,0,64) 
-    uiplot.qwtPlot_2.setAxisScale(uiplot.qwtPlot_2.yLeft, 0, 500) 
-    
+    uiplot.qwtPlot.setAxisScale(uiplot.qwtPlot.xBottom, 0, 500)
+    uiplot.qwtPlot.setAxisScale(uiplot.qwtPlot.yLeft, 3000, 6000)
+    uiplot.qwtPlot_2.setAxisScale(uiplot.qwtPlot_2.xBottom, 0, 64)
+    uiplot.qwtPlot_2.setAxisScale(uiplot.qwtPlot_2.yLeft, 0, 1e6)
+    # uiplot.qwtPlot_2.setAxisAutoScale(1)
+
     uiplot.timer = QtCore.QTimer()
     uiplot.timer.start(1.0)
-    
-   
-   
-    
-    
-    
-    
 
-    ### DISPLAY WINDOWS
+    # DISPLAY WINDOWS
     win_plot.show()
-    code=app.exec_()
-    #epoc.close()
+    code = app.exec_()
+    # epoc.close()
+    # epoc.continuousEnd()
     sys.exit(code)
-
 
 
 if __name__ == "__main__":
     main()
-    
--- GUI_main/recorder.py
+++ GUI_main/recorder.py
@@ -1,7 +1,7 @@
 from pyemotiv import Epoc
 
 import matplotlib
-matplotlib.use('TkAgg') # <-- THIS MAKES IT FAST!
+matplotlib.use('TkAgg')  # <-- THIS MAKES IT FAST!
 import numpy
 from scipy import signal
 import scipy
@@ -13,94 +13,72 @@ import struct
 import matplotlib.mlab as mlab
 
 
-
-
 class EEGRecorder:
-    """Simple, cross-platform class to record from the microphone."""
-    
-    def __init__(self):
 
-        self.threadsDieNow=False #threading 
-        self.newRecord=False
-        
-        
-    ### RECORDING THE EEG stream ###  
-    
-    #get the EEG signal. Getting the raw EEG data and appending to ys
-           
-    
-    #want the app to record continuously, but only plot when a button is pressed
+    """Simple, cross-platform class to record from the microphone."""
+
+    def __init__(self, chan=9):
+
+        self.threadsDieNow = False  # threading
+        self.newRecord = False
+        self.ys = numpy.array([0 for i in xrange(0, 512)])
+        self.chan = chan
+
+    # RECORDING THE EEG stream ###
+    # get the EEG signal. Getting the raw EEG data and appending to ys
+    # want the app to record continuously, but only plot when a button is
+    # pressed
     def continuousStart(self):
         """CALL THIS to start running forever."""
-        #print "Holla"
+        # print "Holla"
         self.t = threading.Thread(target=self.record)
-        
-        self.t.start() #starts the thread
-        
-    #kill the thread
+
+        self.t.start()  # starts the thread
+
+    # kill the thread
     def continuousEnd(self):
         """shut down continuous recording."""
         print "Die!"
-        self.threadsDieNow=True  
+        self.threadsDieNow = True
 
-    
-    def record(self,forever=True): 
-          #need to find a way to empty the array or get it to read from
-        while True: 
-            if self.threadsDieNow: break
-            global datarray
-            datarray=numpy.zeros((1,4))
-            
-           # print"Lookie"
-            
-            epocrec=Epoc()
-            
-            data= epocrec.aquire([9]) #gets raw data from channel O1
-            datarray = numpy.concatenate((datarray, data), axis = 1)
-            self.newRecord=True    
+    def record(self, forever=True):
+        while True:
+            if self.threadsDieNow:
+                break
+
+            datarray = numpy.zeros((1, 4))
+
+            # print"Lookie"
+
+            # epocrec=Epoc()
+
+            data = numpy.random.randint(4000, 5000, size=(1, 4))
+            # data= epocrec.aquire([self.chan]) #gets raw data from channel O1
+            datarray = numpy.concatenate((datarray, data), axis=1)
+            self.newRecord = True
             #print (datarray[0][4], datarray[0][5], datarray[0][6], datarray[0][7])
-            global temp
-            temp= []*4
-            for i in range(0, len(datarray[0])):
-                if datarray[0][i]>0 : 
-                    temp.append(datarray[0][i]) 
 
-           
-            global ys
-            ys = temp
-            #print ys
-            if forever==False: break
-            #print temp.shape
+            temp = [] * 4
+            for i in xrange(0, len(datarray[0])):
+                if datarray[0][i] > 0:
+                    temp.append(datarray[0][i])
 
-    def rawdata(self): 
-       
-        #change the way this is visualized. The logic behind it needs to be tweaked
-       # print temp
-        xs=numpy.arange(0,500)
-        global ys
-        print ys
-        
-        #print (rand)
-        ys=numpy.roll(ys,-1)
+            self.ys = numpy.roll(self.ys, -4)
+            for i in xrange(0, 4):
+                self.ys[508 + i] = temp[i]
 
-        
-        return xs,ys
+            if not forever:
+                break
 
-        ### MATH ###
-    
-    def fft(self): 
-       fs=128
-       pwr,freqs= mlab.psd(temp, Fs=fs,scale_by_freq= False )
-       #pwr= 10*np.log10(np.abs(pwr))
-    
+        # MATH ###
 
-       return freqs, pwr
+    def fft(self):
+        fs = 128
+        pwr, freqs = mlab.psd(self.ys, Fs=fs, scale_by_freq=False)
+        # pwr= 10*numpy.log10(numpy.abs(pwr))
 
-    def batt(self): 
-        epocrec=Epoc()
-        print epocrec.BatteryCharge
+        # lolol high pass
+        for i in xrange(0, 2):
+            pwr[i] = 0
 
-        
-
-        
-    
+        return freqs, pwr
